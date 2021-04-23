6aae0bb1a582d45c1b09a438d8284ec39f6048c8
reyu <ka15sugar@gmail.com>
2021-04-23 23:26:52 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const WEBHOOK_SECRET = getProp('WEBHOOK_SECRET')
 function doPost(e: GoogleAppsScript.Events.DoPost) {
   const params: AlertRequest = JSON.parse(e.postData.contents)
   const description = params.message ? `${params.message}\n` : `\n`
-  const text = `## [${params.title}](${params.ruleUrl})\n` + `${description}`
+  const text = 'this is a test message' //`## [${params.title}](${params.ruleUrl})\n` + `${description}`
   sendMessage(text)
 }
 
